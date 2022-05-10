e145ff9123acd324f60f4cf64b1299670fde447a
kristofvad <kristof.vad@gmail.com>
2022-05-10 10:39:11 +0200

    Fixing html

---
+++ web/src/main/java/hu/nye/progkor/cinema/converter/MovieRequestToDtoConverter.java
@@ -0,0 +1,25 @@
+package hu.nye.progkor.cinema.converter;
+
+import hu.nye.progkor.cinema.model.dto.MovieDTO;
+import hu.nye.progkor.cinema.model.request.MovieRequest;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MovieRequestToDtoConverter implements Converter<MovieRequest, MovieDTO> {
+
+    @Override
+    public MovieDTO convert(@NonNull final MovieRequest source) {
+        log.info("Convert MovieRequest:{} to MovieDTO.", source);
+        return new MovieDTO(null,
+                source.getName(),
+                source.getReleaseYear(),
+                source.getTime(),
+                source.getMovieType(),
+                source.getDescription()
+        );
+    }
+}
